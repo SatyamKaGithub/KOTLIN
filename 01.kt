8540f5bc77d63b442071a846635a82b6c8d00666
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-10-18 19:34:36 +0530

    Create 01.kt

---
+++ 01.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val scoreLevel1 = 79 
+    val scoreLevel2 = 92 
+    val scoreLevel3 = 86
+    
+    val totalScore = scoreLevel1 + scoreLevel2 + scoreLevel3 
+    val averageScore = totalScore / 3 
+ 
+    println(totalScore) 
+    println(averageScore) 
+ 
+    val boostMultiplier = 4 
+    val scoreBoost = totalScore * boostMultiplier 
+    val finalBoostedScore = totalScore + scoreBoost
+    
+    println(scoreBoost) 
+    println(finalBoostedScore) 
+}
